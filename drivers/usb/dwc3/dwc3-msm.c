344816bc6529babbb8346407f9b29ee3227f7976
mohamed.khadri <mohamed.khadri@lge.com>
2013-03-14 17:36:51 -0700

    usb: dwc3-msm: add OF property for adc thresholds
    Change-Id: I8222bfd9248ae2ca3d794692ffee42f9c133e638

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -3045,6 +3045,18 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 	if (ret)
 		dev_dbg(&pdev->dev, "unable to read scm device id (%d)\n", ret);
 
+	if (!of_property_read_u32(node, "qcom,dwc-usb3-msm-adc-low-threshold",
+					&adc_low_threshold)) {
+		dev_info(&pdev->dev,
+			"Read platform data for adc low threshold\n");
+	}
+
+	if (!of_property_read_u32(node, "qcom,dwc-usb3-msm-adc-high-threshold",
+					&adc_high_threshold)) {
+		dev_info(&pdev->dev,
+			"Read platform data for adc high threshold\n");
+	}
+
 	if (of_property_read_u32(node, "qcom,dwc-usb3-msm-tx-fifo-size",
 				 &mdwc->tx_fifo_size))
 		dev_err(&pdev->dev,
