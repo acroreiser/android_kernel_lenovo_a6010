bbe554cdb81b1cc18770d3166d0eaf4bc788a943
Eric Dumazet <edumazet@google.com>
2013-05-15 19:25:55 +0000

    tcp: speedup tcp_fixup_rcvbuf()
    tcp_fixup_rcvbuf() contains a loop to estimate initial socket
    rcv space needed for a given mss. With large MTU (like 64K on lo),
    we can loop ~500 times and consume a lot of cpu cycles.
    
    perf top of 200 concurrent netperf -t TCP_CRR
    
    5.62%  netperf  [kernel.kallsyms]  [k] tcp_init_buffer_space
    1.71%  netperf  [kernel.kallsyms]  [k] _raw_spin_lock
    1.55%  netperf  [kernel.kallsyms]  [k] kmem_cache_free
    1.51%  netperf  [kernel.kallsyms]  [k] tcp_transmit_skb
    1.50%  netperf  [kernel.kallsyms]  [k] tcp_ack
    
    Lets use a 100% factor, and remove the loop.
    
    100% is needed anyway for tcp_adv_win_scale=1
    default value, and is also the maximum factor.
    
    Refs: commit b49960a05e32
          ("tcp: change tcp_adv_win_scale and tcp_rmem[2]")
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Neal Cardwell <ncardwell@google.com>
    Cc: Yuchung Cheng <ycheng@google.com>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -363,9 +363,7 @@ static void tcp_fixup_rcvbuf(struct sock *sk)
 	if (mss > 1460)
 		icwnd = max_t(u32, (1460 * icwnd) / mss, 2);
 
-	rcvmem = SKB_TRUESIZE(mss + MAX_TCP_HEADER);
-	while (tcp_win_from_space(rcvmem) < mss)
-		rcvmem += 128;
+	rcvmem = 2 * SKB_TRUESIZE(mss + MAX_TCP_HEADER);
 
 	rcvmem *= icwnd;
 
