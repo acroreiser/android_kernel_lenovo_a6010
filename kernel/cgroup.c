fe6222c64f1db852c3e7007aae30602de02e7830
Tejun Heo <tj@kernel.org>
2013-06-28 16:24:11 -0700

    cgroup: update error handling in cgroup_populate_dir()
    cgroup_populate_dir() didn't use to check whether the actual file
    creations were successful and could return success with only subset of
    the requested files created, which is nasty.
    
    This patch udpates cgroup_populate_dir() so that it either succeeds
    with all files or fails with no file.
    
    v2: The original patch also converted for_each_root_subsys() usages to
        for_each_subsys() without explaining why.  That part has been
        moved to a separate patch.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: Li Zefan <lizefan@huawei.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -4164,10 +4164,13 @@ static struct cftype cgroup_base_files[] = {
  * cgroup_populate_dir - create subsys files in a cgroup directory
  * @cgrp: target cgroup
  * @subsys_mask: mask of the subsystem ids whose files should be added
+ *
+ * On failure, no file is added.
  */
 static int cgroup_populate_dir(struct cgroup *cgrp, unsigned long subsys_mask)
 {
 	struct cgroup_subsys *ss;
+	int ret = 0;
 
 	/* process cftsets of each subsystem */
 	for_each_root_subsys(cgrp->root, ss) {
@@ -4175,8 +4178,11 @@ static int cgroup_populate_dir(struct cgroup *cgrp, unsigned long subsys_mask)
 		if (!test_bit(ss->subsys_id, &subsys_mask))
 			continue;
 
-		list_for_each_entry(set, &ss->cftsets, node)
-			cgroup_addrm_files(cgrp, ss, set->cfts, true);
+		list_for_each_entry(set, &ss->cftsets, node) {
+			ret = cgroup_addrm_files(cgrp, ss, set->cfts, true);
+			if (ret < 0)
+				goto err;
+		}
 	}
 
 	/* This cgroup is ready now */
@@ -4194,6 +4200,9 @@ static int cgroup_populate_dir(struct cgroup *cgrp, unsigned long subsys_mask)
 	}
 
 	return 0;
+err:
+	cgroup_clear_dir(cgrp, subsys_mask);
+	return ret;
 }
 
 static void css_dput_fn(struct work_struct *work)
