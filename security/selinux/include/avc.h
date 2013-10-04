43776d7a8dda307f2f93b9f01219dc4063673768
Linus Torvalds <torvalds@linux-foundation.org>
2013-10-04 14:05:38 -0700

    selinux: remove 'flags' parameter from avc_audit()
    Now avc_audit() has no more users with that parameter. Remove it.
    
    Change-Id: Ie9a1565b1d1ea0a4a8d17e0174094ff40bd6e904
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- security/selinux/include/avc.h
+++ security/selinux/include/avc.h
@@ -133,7 +133,7 @@ static inline int avc_audit(u32 ssid, u32 tsid,
 			    u16 tclass, u32 requested,
 			    struct av_decision *avd,
 			    int result,
-			    struct common_audit_data *a, unsigned flags)
+			    struct common_audit_data *a)
 {
 #ifdef CONFIG_AUDIT
 	u32 audited, denied;
@@ -142,7 +142,7 @@ static inline int avc_audit(u32 ssid, u32 tsid,
 		return 0;
 	return slow_avc_audit(ssid, tsid, tclass,
 			      requested, audited, denied, result,
-			      a, flags);
+			      a, 0);
 #else
 	return 0;
 #endif
