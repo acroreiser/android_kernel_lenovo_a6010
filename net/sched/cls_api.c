a601403ac0573f02961c7072a79810b79824f1e2
Jamal Hadi Salim <jhs@mojatatu.com>
2013-12-23 08:02:12 -0500

    net_sched: Remove unnecessary checks for act->ops
    Remove unnecessary checks for act->ops
    (suggested by Eric Dumazet).
    
    Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Change-Id: I478de807956e631bc729e0b9aeecce2b90141fcf

--- net/sched/cls_api.c
+++ net/sched/cls_api.c
@@ -587,7 +587,7 @@ int tcf_exts_dump(struct sk_buff *skb, struct tcf_exts *exts,
 		} else if (map->police) {
 			struct tc_action *act = tcf_exts_first_act(exts);
 			nest = nla_nest_start(skb, map->police);
-			if (nest == NULL)
+			if (nest == NULL || !act)
 				goto nla_put_failure;
 			if (tcf_action_dump_old(skb, act, 0, 0) < 0)
 				goto nla_put_failure;
