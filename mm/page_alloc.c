aa56c60dc8b924f78830f88a4b33769704a7c1c7
Mel Gorman <mgorman@suse.de>
2014-08-06 16:07:20 -0700

    mm: page_alloc: abort fair zone allocation policy when remotes nodes are encountered
    The purpose of numa_zonelist_order=zone is to preserve lower zones for
    use with 32-bit devices.  If locality is preferred then the
    numa_zonelist_order=node policy should be used.
    
    Unfortunately, the fair zone allocation policy overrides this by
    skipping zones on remote nodes until the lower one is found.  While this
    makes sense from a page aging and performance perspective, it breaks the
    expected zonelist policy.  This patch restores the expected behaviour
    for zone-list ordering.
    
    Signed-off-by: Mel Gorman <mgorman@suse.de>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2107,7 +2107,7 @@ zonelist_scan:
 		 */
 		if (alloc_flags & ALLOC_FAIR) {
 			if (!zone_local(preferred_zone, zone))
-				continue;
+				break;
 			if (zone_page_state(zone, NR_ALLOC_BATCH) <= 0)
 				continue;
 		}
