4a6529441f967adf2f7680afe5b85edaaa769ff6
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-11-18 13:19:39 +0530

    sched: Avoid frequent migration of running task
    Power values for cpus can drop quite considerably when it goes idle.
    As a result, the best choice for running a single task in a cluster
    can vary quite rapidly. As the task keeps hopping cpus, other cpus go
    idle and start being seen as more favorable target for running a task,
    leading to task migrating almost every scheduler tick!
    
    Prevent this by keeping track of when a task started running on a cpu
    and allowing task migration in tick path (migration_needed()) on
    account of energy efficiency reasons only if the task has run
    sufficiently long (as determined by sysctl_sched_min_runtime
    variable).
    
    Note that currently sysctl_sched_min_runtime setting is considered
    only in scheduler_tick()->migration_needed() path and not in
    idle_balance() path. In other words, a task could be migrated to
    another cpu which did a idle_balance(). This limitation should not
    affect high-frequency migrations seen typically (when a single
    high-demand task runs on high-performance cpu).
    
    CRs-Fixed: 756570
    Change-Id: I96413b7a81b623193c3bbcec6f3fa9dfec367d99
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -377,6 +377,13 @@ static struct ctl_table kern_table[] = {
 		.proc_handler   = sched_window_update_handler,
 	},
 	{
+		.procname	= "sched_min_runtime",
+		.data		= &sysctl_sched_min_runtime,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
+	{
 		.procname       = "sched_heavy_task",
 		.data           = &sysctl_sched_heavy_task_pct,
 		.maxlen         = sizeof(unsigned int),
