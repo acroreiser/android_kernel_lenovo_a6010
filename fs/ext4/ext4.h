4816a0836eb3a7ee5aaa06fb3bd21e4739da94b4
Theodore Ts'o <tytso@google.com>
2015-09-17 16:09:38 -0400

    ext4 crypto: encrypt tmpfile located in encryption protected directory
    Factor out calls to ext4_inherit_context() and move them to
    __ext4_new_inode(); this fixes a problem where ext4_tmpfile() wasn't
    calling calling ext4_inherit_context(), so the temporary file wasn't
    getting protected.  Since the blocks for the tmpfile could end up on
    disk, they really should be protected if the tmpfile is created within
    the context of an encrypted directory.
    
    Change-Id: I367815239a5301e2fe703de62f3fdb29ee0e2fbc
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: "Theodore Ts'o" <tytso@google.com>

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -2253,6 +2253,11 @@ static inline int ext4_get_encryption_info(struct inode *inode)
 	return 0;
 }
 
+static inline struct ext4_crypt_info *ext4_encryption_info(struct inode *inode)
+{
+	return EXT4_I(inode)->i_crypt_info;
+}
+
 #else
 static inline int ext4_has_encryption_key(struct inode *inode)
 {
@@ -2261,6 +2266,10 @@ static inline int ext4_has_encryption_key(struct inode *inode)
 static inline int ext4_get_encryption_info(struct inode *inode)
 {
 	return 0;
+}
+static inline struct ext4_crypt_info *ext4_encryption_info(struct inode *inode)
+{
+	return NULL;
 }
 #endif
 
