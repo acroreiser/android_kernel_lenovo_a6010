4a8c1d6d626c95c179f27afb71b04af4e3acaa99
Amit Pundir <amit.pundir@linaro.org>
2015-12-21 16:01:10 +0530

    cgroup: refactor allow_attach handler for 4.4
    Refactor *allow_attach() handler to align it with the changes
    from mainline commit 1f7dd3e5a6e4 "cgroup: fix handling of
    multi-destination migration from subtree_control enabling".
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Change-Id: I2b1bc62a4d8fb5558d5c9e467d48fafca7dad066

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -6998,6 +6998,11 @@ static int mem_cgroup_can_attach(struct cgroup_subsys_state *css,
 	return ret;
 }
 
+static int mem_cgroup_allow_attach(struct cgroup_taskset *tset)
+{
+	return subsys_cgroup_allow_attach(tset);
+}
+
 static void mem_cgroup_cancel_attach(struct cgroup_taskset *tset)
 {
 	mem_cgroup_clear_mc();
@@ -7164,6 +7169,10 @@ static int mem_cgroup_can_attach(struct cgroup_taskset *tset)
 {
 	return 0;
 }
+static int mem_cgroup_allow_attach(struct cgroup_taskset *tset)
+{
+	return 0;
+}
 static void mem_cgroup_cancel_attach(struct cgroup_taskset *tset)
 {
 }
@@ -7196,6 +7205,7 @@ struct cgroup_subsys memory_cgrp_subsys = {
 	.can_attach = mem_cgroup_can_attach,
 	.cancel_attach = mem_cgroup_cancel_attach,
 	.attach = mem_cgroup_move_task,
+	.allow_attach = mem_cgroup_allow_attach,
 	.bind = mem_cgroup_bind,
 	.legacy_cftypes = mem_cgroup_files,
 	.early_init = 0,
