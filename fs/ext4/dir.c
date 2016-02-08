b2d3a30547fb5c152fb7f4d8f797171b05db51a8
Theodore Ts'o <tytso@mit.edu>
2016-02-07 19:35:05 -0500

    UPSTREAM: ext4 crypto: revalidate dentry after adding or removing the key
    commit 28b4c263961c47da84ed8b5be0b5116bad1133eb upstream.
    
    Add a validation check for dentries for encrypted directory to make
    sure we're not caching stale data after a key has been added or removed.
    
    Also check to make sure that status of the encryption key is updated
    when readdir(2) is executed.
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 2faff9d1dfc560da3bfba6a8ab0c30246fd6c1cb)
    Change-Id: I9b77145c13b70df8dec5a90c977d6dc98d9989ae
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- fs/ext4/dir.c
+++ fs/ext4/dir.c
@@ -123,6 +123,12 @@ static int ext4_readdir(struct file *filp,
 	int dir_has_error = 0;
 	struct ext4_str fname_crypto_str = {.name = NULL, .len = 0};
 
+	if (ext4_encrypted_inode(inode)) {
+		err = ext4_get_encryption_info(inode);
+		if (err && err != -ENOKEY)
+			return err;
+	}
+
 	if (is_dx_dir(inode)) {
 		err = ext4_dx_readdir(filp, dirent, filldir);
 		if (err != ERR_BAD_DX_DIR) {
