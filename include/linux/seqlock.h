71e33f9130c897a5ce729a7dbbb01f41d89a2811
Alexey Dobriyan <adobriyan@gmail.com>
2016-05-26 15:16:06 -0700

    seqlock: fix raw_read_seqcount_latch()
    lockless_dereference() is supposed to take pointer not integer.
    
    Link: http://lkml.kernel.org/r/20160521201448.GA7429@p183.telecom.by
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/seqlock.h
+++ include/linux/seqlock.h
@@ -188,7 +188,7 @@ static inline void write_seqcount_end(seqcount_t *s)
 
 static inline int raw_read_seqcount_latch(seqcount_t *s)
 {
-	return lockless_dereference(s->sequence);
+	return lockless_dereference(s)->sequence;
 }
 
 /**
