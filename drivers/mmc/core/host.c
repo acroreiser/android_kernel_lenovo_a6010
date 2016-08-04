840521b5df2bb5e387a8abb75c56a2686d241d8d
Mohan Srinivasan <srmohan@google.com>
2016-08-04 15:17:29 -0700

    Android: MMC IO Latency Histograms.
    This patch adds a new sysfs node (latency_hist) and reports IO
    (svc time) latency histograms. Disabled by default, can be enabled
    by echoing 0 into latency_hist, stats can be cleared by writing
    2 into latency_hist.
    
    Change-Id: I26c77a63af39417812e3989c836b4b5b684dfa85
    Signed-off-by: Mohan Srinivasan <srmohan@google.com>
    Bug: 30677035

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -32,8 +32,6 @@
 #include "core.h"
 #include "host.h"
 
-#define cls_dev_to_mmc_host(d)	container_of(d, struct mmc_host, class_dev)
-
 static void mmc_host_classdev_release(struct device *dev)
 {
 	struct mmc_host *host = cls_dev_to_mmc_host(dev);
@@ -941,6 +939,8 @@ int mmc_add_host(struct mmc_host *host)
 		pr_err("%s: failed to create sysfs group with err %d\n",
 							 __func__, err);
 
+	mmc_latency_hist_sysfs_init(host);
+
 	mmc_start_host(host);
 	if (!(host->pm_flags & MMC_PM_IGNORE_PM_NOTIFY))
 		register_pm_notifier(&host->pm_notify);
@@ -992,7 +992,7 @@ void mmc_free_host(struct mmc_host *host)
 	idr_remove(&mmc_host_idr, host->index);
 	spin_unlock(&mmc_host_lock);
 	wake_lock_destroy(&host->detect_wake_lock);
-
+	mmc_latency_hist_sysfs_exit(host);
 	put_device(&host->class_dev);
 }
 
