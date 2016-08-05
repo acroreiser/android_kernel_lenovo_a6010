5aa7beaf68993c93ed0d3533dbbb32146b205832
Paolo Valente <paolo.valente@linaro.org>
2016-08-05 19:52:54 +0200

    BFQ v8r1

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -4890,7 +4890,7 @@ static struct elevator_type iosched_bfq = {
 static int __init bfq_init(void)
 {
 	int ret;
-	char msg[50] = "BFQ I/O-scheduler: v8";
+	char msg[50] = "BFQ I/O-scheduler: v8r1";
 
 	/*
 	 * Can be 0 on HZ < 1000 setups.
