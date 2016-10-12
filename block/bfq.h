fac7a69c68146b5a6d118d73115a325b2571f7c7
Paolo Valente <paolo.valente@linaro.org>
2016-10-12 22:01:12 +0200

    Guarantee that wr is not lost for a spurious temporary merge
    If a queue Q1 is merged with an already-shared queue Q2, then Q1
    may easily be freed soon after this event. If this happens and
    the merge lasts little, then Q1 may be created again at a time
    instant at which it had still to enjoy weight raising. This
    commit checks that, and restores weight raising for the queue
    if it is the case.
    
    Signed-off-by: Paolo Valente <paolo.valente@linaro.org>

--- block/bfq.h
+++ block/bfq.h
@@ -347,6 +347,12 @@ struct bfq_io_cq {
 
 	bool saved_in_large_burst;
 	bool was_in_burst_list;
+
+	/*
+	 * Similar to previous fields: save wr information.
+	 */
+	unsigned long saved_wr_coeff;
+	unsigned long saved_last_wr_start_finish;
 };
 
 enum bfq_device_speed {
