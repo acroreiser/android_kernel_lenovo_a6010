6b59555f2c0206e4c691d27a5577d987c2f5f4d2
Viresh Kumar <viresh.kumar@linaro.org>
2017-02-21 10:15:18 +0530

    FROM-LIST: cpufreq: schedutil: Redefine the rate_limit_us tunable
    The rate_limit_us tunable is intended to reduce the possible overhead
    from running the schedutil governor.  However, that overhead can be
    divided into two separate parts: the governor computations and the
    invocation of the scaling driver to set the CPU frequency.  The latter
    is where the real overhead comes from.  The former is much less
    expensive in terms of execution time and running it every time the
    governor callback is invoked by the scheduler, after rate_limit_us
    interval has passed since the last frequency update, would not be a
    problem.
    
    For this reason, redefine the rate_limit_us tunable so that it means the
    minimum time that has to pass between two consecutive invocations of the
    scaling driver by the schedutil governor (to set the CPU frequency).
    
    Change-Id: Iced64116b826c25441ef537c27a3dabfcf81919e
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    [pulled from linux-pm linux-next https://patchwork.kernel.org/patch/9583949/ ]
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -117,10 +117,9 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 	if (sugov_up_down_rate_limit(sg_policy, time, next_freq))
 		return;
 
-	sg_policy->last_freq_update_time = time;
-
 	if (sg_policy->next_freq != next_freq) {
 		sg_policy->next_freq = next_freq;
+		sg_policy->last_freq_update_time = time;
 		sg_policy->work_in_progress = true;
 		irq_work_queue(&sg_policy->irq_work);
 	}
