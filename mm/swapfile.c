b5cdd78cbd1d080f732abfe564036776a5ef62f6
Jaroslav Zvezda <acroreiser@gmail.com>
2018-05-01 15:58:18 +0300

    mm: swap: allow swap only on zram partition

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -2124,6 +2124,9 @@ SYSCALL_DEFINE2(swapon, const char __user *, specialfile, int, swap_flags)
 		return PTR_ERR(p);
 
 	name = getname(specialfile);
+	if(strcmp(name, "/dev/block/zram0"))
+		return -EPERM;
+
 	if (IS_ERR(name)) {
 		error = PTR_ERR(name);
 		name = NULL;
