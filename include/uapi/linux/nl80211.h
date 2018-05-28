c89c35b909273cc04539407bf0a8a361181976ed
Peng Xu <pxu@codeaurora.org>
2018-05-28 14:50:02 -0700

    nl80211: add an option to allow MFP without requiring it
    The user space can now allow the kernel to associate to an AP that
    requires MFP or that doesn't have MFP enabled in the same
    NL80211_CMD_CONNECT command, by using a new NL80211_MFP_OPTIONAL flag.
    The driver / firmware will decide whether to use it or not.
    
    Include a feature bit to advertise support for NL80211_MFP_OPTIONAL.
    This allows new user space to run on old kernels and know that it
    cannot use the new attribute if it isn't supported.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Change-Id: I8dc2c341e44ccf9877ebfcbf645eb2d00c58222c
    Git-commit: 65026002d69de006e273749bb799d3b01b757eb0
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/jberg/mac80211-next.git
    CRs-fixed: 2173869
    Signed-off-by: Peng Xu <pxu@codeaurora.org>

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -1161,8 +1161,12 @@ enum nl80211_commands {
  *
  * @NL80211_ATTR_USE_MFP: Whether management frame protection (IEEE 802.11w) is
  *	used for the association (&enum nl80211_mfp, represented as a u32);
- *	this attribute can be used
- *	with %NL80211_CMD_ASSOCIATE and %NL80211_CMD_CONNECT requests
+ *	this attribute can be used with %NL80211_CMD_ASSOCIATE and
+ *	%NL80211_CMD_CONNECT requests. %NL80211_MFP_OPTIONAL is not allowed for
+ *	%NL80211_CMD_ASSOCIATE since user space SME is expected and hence, it
+ *	must have decided whether to use management frame protection or not.
+ *	Setting %NL80211_MFP_OPTIONAL with a %NL80211_CMD_CONNECT request will
+ *	let the driver (or the firmware) decide whether to use MFP or not.
  *
  * @NL80211_ATTR_STA_FLAGS2: Attribute containing a
  *	&struct nl80211_sta_flag_update.
@@ -3281,10 +3285,12 @@ enum nl80211_key_type {
  * enum nl80211_mfp - Management frame protection state
  * @NL80211_MFP_NO: Management frame protection not used
  * @NL80211_MFP_REQUIRED: Management frame protection required
+ * @NL80211_MFP_OPTIONAL: Management frame protection is optional
  */
 enum nl80211_mfp {
 	NL80211_MFP_NO,
 	NL80211_MFP_REQUIRED,
+	NL80211_MFP_OPTIONAL,
 };
 
 enum nl80211_wpa_versions {
@@ -4042,6 +4048,8 @@ enum nl80211_feature_flags {
 /**
  * enum nl80211_ext_feature_index - bit index of extended features.
  *
+ * @NL80211_EXT_FEATURE_MFP_OPTIONAL: Driver supports the %NL80211_MFP_OPTIONAL
+ *  value in %NL80211_ATTR_USE_MFP.
  * @NL80211_EXT_FEATURE_AP_PMKSA_CACHING: Driver/device supports PMKSA caching
  *	(set/del PMKSA operations) in AP mode.
  *
