bf97dea5a24288325077e2a3b0f1c85c432e2775
pierre <pdbbasketball@gmail.com>
2018-09-01 12:59:53 -0400

    kernel: Boost cpufreq when userspace launches an app
    Zygote is Android's application process, and it only forks when launching
    a new app for the first time. To accelerate app launches, boost cpufreq
    to the max for 1250 ms when zygote decides to fork.

--- kernel/fork.c
+++ kernel/fork.c
@@ -72,6 +72,7 @@
 #include <linux/uprobes.h>
 #include <linux/aio.h>
 #include <linux/cpufreq.h>
+#include <linux/cpu_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -1650,6 +1651,10 @@ long do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+
+	/* Boost devfreq device to the max for 1250 ms when userspace launches an app */
+	if (is_zygote_pid(current->pid))
+		do_input_boost_max();
 
 	/*
 	 * Do some preliminary argument and permissions checking before we
