c956a34365c3e47463212f36e576eaf875c49ff6
Jaroslav Zvezda <acroreiser@gmail.com>
2019-01-07 17:39:00 +0200

    some things from upstream

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -168,6 +168,9 @@ extern unsigned long nr_running(void);
 extern unsigned long nr_iowait(void);
 extern unsigned long nr_iowait_cpu(int cpu);
 extern unsigned long this_cpu_load(void);
+#ifdef CONFIG_CPU_QUIET
+extern u64 nr_running_integral(unsigned int cpu);
+#endif
 
 extern void sched_update_nr_prod(int cpu, long delta, bool inc);
 extern void sched_get_nr_running_avg(int *avg, int *iowait_avg, int *big_avg);
