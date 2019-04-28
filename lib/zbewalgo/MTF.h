d639cee55afa143629faf3b460cba42cdb1a5092
Jaroslav Zvezda <acroreiser@gmail.com>
2019-04-29 02:56:52 +0300

    Revert "Revert "add compression algorithm zBeWalgo""
    This reverts commit 76416c9b0fba603024f7f64485a12a9c5c6548bd.

---
+++ lib/zbewalgo/MTF.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2018 Benjamin Warnke <4bwarnke@informatik.uni-hamburg.de>
+ */
+
+#ifndef ZBEWALGO_MTF_H
+#define ZBEWALGO_MTF_H
+
+#include "include.h"
+
+extern struct zbewalgo_alg alg_mtf;
+
+#endif
