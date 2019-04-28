9ee170ae98303f0be7f67c15bbfa504cbf6ad14a
Jaroslav Zvezda <acroreiser@gmail.com>
2019-04-29 02:56:52 +0300

    Revert "Revert "add compression algorithm zBeWalgo""
    This reverts commit 76416c9b0fba603024f7f64485a12a9c5c6548bd.

---
+++ lib/zbewalgo/bewalgo.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2018 Benjamin Warnke <4bwarnke@informatik.uni-hamburg.de>
+ */
+
+#ifndef ZBEWALGO_BEWALGO_H
+#define ZBEWALGO_BEWALGO_H
+
+#include "include.h"
+
+extern struct zbewalgo_alg alg_bewalgo;
+
+#endif
