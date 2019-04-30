a4f48a5dc4c98dbfcdc847767c3e2f31fc1863c3
Jaroslav Zvezda <acroreiser@gmail.com>
2019-04-30 21:52:10 +0300

    msm_limiter: simplify

--- include/soc/qcom/limiter.h
+++ include/soc/qcom/limiter.h
@@ -22,10 +22,7 @@
 static struct cpu_limit {
 	unsigned int limiter_enabled;
 	uint32_t suspend_max_freq;
-	uint32_t resume_max_freq_all;
-	uint32_t resume_max_freq[4];
-	uint32_t suspend_min_freq_all;
-	uint32_t suspend_min_freq[4];
+	uint32_t suspend_min_freq;
 	unsigned int suspended;
 	unsigned int suspend_defer_time;
 	struct delayed_work suspend_work;
@@ -36,9 +33,6 @@ static struct cpu_limit {
 } limit = {
 	.limiter_enabled = LIMITER_ENABLED,
 	.suspend_max_freq = DEFAULT_SUSPEND_FREQUENCY,
-	.resume_max_freq_all = DEFAULT_RESUME_FREQUENCY,
-	.resume_max_freq[0] = DEFAULT_RESUME_FREQUENCY,
-	.suspend_min_freq_all = DEFAULT_MIN_FREQUENCY,
-	.suspend_min_freq[0] = DEFAULT_MIN_FREQUENCY,
+	.suspend_min_freq = DEFAULT_MIN_FREQUENCY,
 	.suspend_defer_time = DEFAULT_SUSPEND_DEFER_TIME,
 };
