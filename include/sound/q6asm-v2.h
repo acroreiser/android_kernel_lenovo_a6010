zachariasmaladroit/android_kernel_sony_msm8994_kitakami_r2

ASoC: add G711 audio encoding support
G711 PCM is a commonly used waveform speech codec.
add native audio drivers for both G711 mlaw and G711
alaw encoding formats.

CRs-Fixed: 990836
Change-Id: Ia187948fab9840447b1f5b1ede4e9a1ece0b85c5
Signed-off-by: Preetam Singh Ranawat <apranawat@codeaurora.org>

include/sound/q6asm-v2.h
@@ -52,6 +52,8 @@
 #define FORMAT_EAC3         0x0014
 #define FORMAT_MP2          0x0015
 #define FORMAT_FLAC         0x0016
+#define FORMAT_G711_ALAW_FS 0x0017
+#define FORMAT_G711_MLAW_FS 0x0018
 
 #define ENCDEC_SBCBITRATE   0x0001
 #define ENCDEC_IMMEDIATE_DECODE 0x0002
@@ -347,6 +349,10 @@ int q6asm_enc_cfg_blk_aac(struct audio_client *ac,
 			uint32_t sample_rate, uint32_t channels,
 			 uint32_t bit_rate,
 			 uint32_t mode, uint32_t format);
+
+int q6asm_enc_cfg_blk_g711(struct audio_client *ac,
+			 uint32_t frames_per_buf,
+			uint32_t sample_rate);
 
 int q6asm_enc_cfg_blk_pcm(struct audio_client *ac,
 			uint32_t rate, uint32_t channels, bool use_back_flavor);

acroreiser/android_kernel_lenovo_a6010

Signed-off-by: Laster K. (lazerl0rd) <officiallazerl0rd@gmail.com>

include/sound/q6asm-v2.h
@@ -49,6 +49,8 @@
 #define FORMAT_ALAC         0x0017
 #define FORMAT_VORBIS       0x0018
 #define FORMAT_APE          0x0019
+#define FORMAT_G711_ALAW_FS 0x001a
+#define FORMAT_G711_MLAW_FS 0x001b
 
 #define ENCDEC_SBCBITRATE   0x0001
 #define ENCDEC_IMMEDIATE_DECODE 0x0002
@@ -320,6 +322,10 @@ int q6asm_enc_cfg_blk_aac(struct audio_client *ac,
 			uint32_t sample_rate, uint32_t channels,
 			 uint32_t bit_rate,
 			 uint32_t mode, uint32_t format);
+
+int q6asm_enc_cfg_blk_g711(struct audio_client *ac,
+			 uint32_t frames_per_buf,
+			uint32_t sample_rate);
 
 int q6asm_enc_cfg_blk_pcm(struct audio_client *ac,
 			uint32_t rate, uint32_t channels);