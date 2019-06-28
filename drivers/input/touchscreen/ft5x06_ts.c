24f40906a900dbed25286ee05cff3e06035c95b3
dev-harsh1998 <dev-harsh1998@hotmail.com>
2019-06-28 17:52:43 +0530

    drivers: input: ft5x06: fix driver build
    * pointer to ft5x06_ts is used later in the parts of code.
    
    Signed-off-by: dev-harsh1998 <dev-harsh1998@hotmail.com>

--- drivers/input/touchscreen/ft5x06_ts.c
+++ drivers/input/touchscreen/ft5x06_ts.c
@@ -278,9 +278,9 @@ struct ft5x06_ts_data {
 static int ft5x06_ts_start(struct device *dev);
 static int ft5x06_ts_stop(struct device *dev);
 
-struct ft5x06_ts_data *screen_suspend_check = NULL;
+struct ft5x06_ts_data *ft5x06_ts = NULL;
 bool is_touch_screen_suspended(void){
-	return screen_suspend_check->suspended;
+	return ft5x06_ts->suspended;
 }
 
 static struct sensors_classdev __maybe_unused sensors_proximity_cdev = {
