df73fcbe68c223e2d067d125a0b64926f8694b21
Jaroslav Zvezda <acroreiser@gmail.com>
2019-06-30 18:25:29 +0300

    ramdisk/koffee.sh: cleanup

--- anykernel_a6000/ramdisk/koffee.sh
+++ anykernel_a6000/ramdisk/koffee.sh
@@ -1,11 +1,11 @@
 #!/system/bin/sh
-# Koffee's (Hyperloop) startup script
+# Koffee's (now Hyperloop's) startup script
 # running immediatelly after mounting /system
 # do not edit!
 
-sleep 15
+# sleep 15
 
-tc qdisc add dev rmnet_data0 root fq_codel
+# currently do nothing #
 
 /sbin/busybox mount -o remount,rw /
 /sbin/busybox rm /koffee.sh
