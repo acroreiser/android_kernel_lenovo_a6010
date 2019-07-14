0d3e5b4048aac1b1aedf2817b34fc424d1db4c51
Jaroslav Zvezda <acroreiser@gmail.com>
2019-07-14 23:14:01 +0300

    Revert "mmc: increase priority of mmcX thread"
    This reverts commit fe47401b38c78117a454a5c2a7911b9cd4edf7c9.

--- drivers/mmc/card/queue.c
+++ drivers/mmc/card/queue.c
@@ -62,7 +62,7 @@ static int mmc_queue_thread(void *d)
 
 	struct sched_param scheduler_params = {0};
 
-	scheduler_params.sched_priority = 10;
+	scheduler_params.sched_priority = 1;
 	sched_setscheduler(current, SCHED_FIFO, &scheduler_params);
 
 	current->flags |= PF_MEMALLOC;
