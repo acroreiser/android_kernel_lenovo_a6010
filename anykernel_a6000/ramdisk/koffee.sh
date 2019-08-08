4eabe0241f9f39312232e8bd8446aa607cf8c90d
Jaroslav Zvezda <acroreiser@gmail.com>
2019-08-08 08:16:38 +0300

    a60x0: setup bfqio cgroup for userspace

--- anykernel_a6000/ramdisk/koffee.sh
+++ anykernel_a6000/ramdisk/koffee.sh
@@ -1,8 +1,19 @@
 #!/system/bin/sh
-# Koffee's (now Hyperloop's) startup script
+# Koffee's (now Infernal's) startup script
 # running immediatelly after mounting /system
 # do not edit!
 
-# currently do nothing #
+mkdir /dev/bfqio
+/sbin/busybox mount -t cgroup none /dev/bfqio -o bfqio
+sleep 1
+mkdir /dev/bfqio/rt-display
+chmod 0664 /dev/bfqio/*
+chmod 0220 /dev/bfqio/cgroup.event_control
+chmod 0664 /dev/bfqio/rt-display/*
+chmod 0755 /dev/bfqio
+chmod 0755 /dev/bfqio/rt-display
+chmod 0220 /dev/bfqio/rt-display/cgroup.event_control
+chown -R root:system /dev/bfqio
 
+echo 1 > /dev/bfqio/rt-display/bfqio.ioprio_class
 exit 0
