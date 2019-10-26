c44fd7c1fa3464f9852ed577347aa7ef716ab58b
Jaroslav Zviezda <acroreiser@gmail.com>
2019-10-26 11:12:29 +0300

    Revert "vfs: restructure d_genocide()"
    This reverts commit 8870f6a19266917afc329e0c6fd2e24823f30567.

--- fs/dcache.c
+++ fs/dcache.c
@@ -3054,10 +3054,6 @@ resume:
 			spin_unlock(&dentry->d_lock);
 			continue;
 		}
-		if (!(dentry->d_flags & DCACHE_GENOCIDE)) {
-			dentry->d_flags |= DCACHE_GENOCIDE;
-			dentry->d_lockref.count--;
-		}
 		if (!list_empty(&dentry->d_subdirs)) {
 			spin_unlock(&this_parent->d_lock);
 			spin_release(&dentry->d_lock.dep_map, 1, _RET_IP_);
@@ -3065,12 +3061,20 @@ resume:
 			spin_acquire(&this_parent->d_lock.dep_map, 0, 1, _RET_IP_);
 			goto repeat;
 		}
+		if (!(dentry->d_flags & DCACHE_GENOCIDE)) {
+			dentry->d_flags |= DCACHE_GENOCIDE;
+			dentry->d_lockref.count--;
+		}
 		spin_unlock(&dentry->d_lock);
 	}
 	rcu_read_lock();
 ascend:
 	if (this_parent != root) {
 		struct dentry *child = this_parent;
+		if (!(this_parent->d_flags & DCACHE_GENOCIDE)) {
+			this_parent->d_flags |= DCACHE_GENOCIDE;
+			this_parent->d_lockref.count--;
+		}
 		this_parent = child->d_parent;
 
 		spin_unlock(&child->d_lock);
