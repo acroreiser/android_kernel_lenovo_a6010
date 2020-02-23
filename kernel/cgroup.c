d98b1f00b29b82bd3713323b7402c01a4849016f
Jaroslav Zviezda <acroreiser@gmail.com>
2020-02-23 22:10:26 +0300

    cgroups,iosched: boost foreground (a.k.a top-) app io priority
    Useful with cfq, bfq or fiops ioscheds.

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -60,6 +60,8 @@
 #include <linux/poll.h>
 #include <linux/flex_array.h> /* used in cgroup_attach_task */
 #include <linux/kthread.h>
+#include <linux/sched/sysctl.h>
+#include <linux/ioprio.h>
 
 #include <linux/atomic.h>
 
@@ -191,6 +193,8 @@ struct cgroup_event {
 	wait_queue_t wait;
 	struct work_struct remove;
 };
+
+unsigned int sysctl_iosched_boost_top_app = 0;
 
 /* The list of hierarchy roots */
 
@@ -2220,6 +2224,13 @@ retry_find_task:
 			sched_setscheduler(tsk, SCHED_IDLE, &param);
 	}
 
+	if (sysctl_iosched_boost_top_app == 1)
+	{
+		if (!memcmp(cgrp->name->name, "top-app", sizeof("top-app")) && tsk->cred->uid > 10000)
+			set_task_ioprio(tsk, IOPRIO_PRIO_VALUE(1, 6));
+		else
+			set_task_ioprio(tsk, IOPRIO_PRIO_VALUE(IOPRIO_CLASS_NONE, IOPRIO_NORM));
+	}
 
 	threadgroup_unlock(tsk);
 
