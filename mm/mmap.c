5f5bff6a7eb76225f976488175baf6516558020a
Jaroslav Zviezda <acroreiser@gmail.com>
2020-04-06 09:52:37 +0300

    Revert "uksm: fix compilation with acd28e9ff072aee7024e07259ad189355bf0c250"
    This reverts commit b5e948cb514edec2a81fd2897d128cf1a4ae90ec.

--- mm/mmap.c
+++ mm/mmap.c
@@ -2820,7 +2820,7 @@ void exit_mmap(struct mm_struct *mm)
 
 	mm->mmap = NULL;
 	mm->mm_rb = RB_ROOT;
-	vmacache_invalidate(mm);
+	mm->mmap_cache = NULL;
 	up_write(&mm->mmap_sem);
 
 	WARN_ON(mm->nr_ptes > (FIRST_USER_ADDRESS+PMD_SIZE-1)>>PMD_SHIFT);
