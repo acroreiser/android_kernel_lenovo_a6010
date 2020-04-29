eb7572388d0f7355656d9e0f21d84d92f11461b8
Jaroslav Zviezda <acroreiser@gmail.com>
2020-04-29 14:14:45 +0300

    Revert "ureadahead: disable"
    This reverts commit c06cd8397eaa13b51b569cdbbb1348930c096213.

--- anykernel_a6000/ramdisk/koffee.sh
+++ anykernel_a6000/ramdisk/koffee.sh
@@ -6,6 +6,11 @@
 # Enavle Laptop mode
 echo 1 > /proc/sys/vm/laptop_mode
 
+# Start ureadahead daemon (ported from Ubuntu)
+# to speed up booting
+mkdir -m 0777 /data/ureadahead
+/persist/infernal/sbin/ureadahead --daemon --timeout=35
+
 # Turn /cache to sync mode
 /persist/infernal/sbin/busybox mount -o remount,sync /cache
 
