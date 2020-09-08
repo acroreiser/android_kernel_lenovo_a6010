a728122f8e8f6a285da8cac63757919d9f9a9c22
Aarqw12 <lcockx@protonmail.com>
2020-09-08 13:20:58 +0300

    prima: import changes *pP2pAdapter and wlan_mac.bin
    *Imported from kernel mi-code " daisy-q-oss "
    
    Change-Id: 01e8cbaa4ed083ffca5cde7a0e42b1ad95311824
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- drivers/staging/prima/CORE/VOSS/src/vos_nvitem.c
+++ drivers/staging/prima/CORE/VOSS/src/vos_nvitem.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012-2018 The Linux Foundation. All rights reserved.
+ * Copyright (C) 2020 XiaoMi, Inc.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -811,6 +812,31 @@ fail:
    return vosStatus;
 }
 
+/* Added by WB20195992 for get mac. -begin */
+VOS_STATUS get_mac_buf(char *pfileName,v_VOID_t *pCtx, char* mac, int len)
+{
+    int status;
+    VOS_STATUS retval = VOS_STATUS_SUCCESS;
+    hdd_context_t *pHddCtx = (hdd_context_t*)pCtx;
+    status = request_firmware(&pHddCtx->fw, pfileName, pHddCtx->parent_dev);
+    if(status || !pHddCtx->fw || !pHddCtx->fw->data) {
+        hddLog(VOS_TRACE_LEVEL_FATAL, "%s: Firmware %s download failed",
+                __func__, pfileName);
+        retval = VOS_STATUS_E_FAILURE;
+    }
+    else {
+        memcpy(mac, (v_VOID_t *)pHddCtx->fw->data, len);
+        hddLog(VOS_TRACE_LEVEL_FATAL, "%s: Firmware size",
+                        __func__);
+        /*hddLog(VOS_TRACE_LEVEL_FATAL, "%s: Firmware size = %d",
+                __func__, pHddCtx->fw->size);*/
+
+        retval = VOS_STATUS_SUCCESS;
+    }
+    return retval;
+}
+/* Added by WB20195992 for get mac. -end */
+
 /**------------------------------------------------------------------------
   \brief vos_nv_parseV2bin() - Parse NV2 binary
          Parse NV2 BIN, and assign contents to common NV structure.
@@ -1162,6 +1188,9 @@ VOS_STATUS vos_nv_open(void)
     v_U32_t dataOffset;
     sHalNv *pnvData = NULL;
     hdd_context_t *pHddCtx = NULL;
+    /* Added by WB20195992 for read mac. -begin */
+    char mac_buf[24];
+    /* Added by WB20195992 for read mac. -end */
 
     /*Get the global context */
     pVosContext = vos_get_global_context(VOS_MODULE_ID_SYS, NULL);
@@ -1374,6 +1403,18 @@ VOS_STATUS vos_nv_open(void)
         /* NV verion is NV2 */
         ((VosContextType*)(pVosContext))->nvVersion = E_NV_V2;
     }
+
+    /* Added by WB20195992 for read mac. -begin */
+    status = get_mac_buf("wlan/prima/wlan_mac.bin", ((VosContextType*)(pVosContext))->pHDDContext, mac_buf, sizeof(mac_buf));
+    //added return status
+    if (VOS_IS_STATUS_SUCCESS( status ))
+    {
+        memcpy(gnvEFSTable->halnv.fields.macAddr, mac_buf, VOS_MAC_ADDRESS_LEN);
+        memcpy(gnvEFSTable->halnv.fields.macAddr2, mac_buf + VOS_MAC_ADDRESS_LEN, VOS_MAC_ADDRESS_LEN);
+        memcpy(gnvEFSTable->halnv.fields.macAddr3, mac_buf + VOS_MAC_ADDRESS_LEN * 2, VOS_MAC_ADDRESS_LEN);
+        memcpy(gnvEFSTable->halnv.fields.macAddr4, mac_buf + VOS_MAC_ADDRESS_LEN * 3, VOS_MAC_ADDRESS_LEN);
+    }
+    /* Added by WB20195992 for read mac. -end */
 
     if (NULL != pnvData)
     {
