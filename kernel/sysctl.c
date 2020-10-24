b1878745e5338f68ea02de783b6085af9e180524
Jaroslav Zviezda <acroreiser@gmail.com>
2020-10-25 01:11:51 +0300

    cgroup/cpusets: Android task priority tuning
    Boosts top app by reducing scheduling priority of background tasks.
    
    Controlled via "tune_android_tasks" sysctl node.
    
    Sysctl tune_android_tasks:
    
    * 0 = disabled (will set already changed priority to normal)
    * 1 = enabled (default)
    * 2 = enabled and set realtime priority to top app (prevents userspace from setting scheduling policy)
    
    Sched & io priority of tasks from "background" will be IDLE.
    Tasks from "foreground" cpuset will get SCHED_BATCH policy with normal io priority.
    
    If tune_android_tasks = 2, tasks from "top-app" cpuset will run with SCHED_FIFO policy.
    
    When tune_android_tasks is 2, userspace will be unable to change scheduling policy of any process.
    
    System apps and services is not affected.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -284,6 +284,15 @@ static int max_extfrag_threshold = 1000;
 #endif
 
 static struct ctl_table kern_table[] = {
+#ifdef CONFIG_ANDROID_TASK_TUNING
+	{
+		.procname	= "tune_android_tasks",
+		.data		= &sysctl_tune_android_tasks,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
+#endif
 	{
 		.procname	= "sched_child_runs_first",
 		.data		= &sysctl_sched_child_runs_first,
