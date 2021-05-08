b7239c1869159dfa0622c62782f5497eb9193bad
Yaroslav Zviezda <acroreiser@gmail.com>
2021-05-08 23:45:41 +0300

    rcu: unbreak nohz_full

--- kernel/rcu/tree_plugin.h
+++ kernel/rcu/tree_plugin.h
@@ -2346,7 +2346,7 @@ static void rcu_kick_nohz_cpu(int cpu)
 #endif /* #ifdef CONFIG_NO_HZ_FULL */
 }
 
-#if 0 // @acroreiser: we don't need this on simplified kernel
+#ifdef CONFIG_NO_HZ_FULL
 /*
  * Bind the grace-period kthread for the sysidle flavor of RCU to the
  * timekeeping CPU.
