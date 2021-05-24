8b4353299f774929853dc891870c5f4df9ab5d59
Suren Baghdasaryan <surenb@google.com>
2021-05-24 12:53:39 -0700

    BACKPORT: cgroup: make per-cgroup pressure stall tracking configurable
    PSI accounts stalls for each cgroup separately and aggregates it at each
    level of the hierarchy. This causes additional overhead with psi_avgs_work
    being called for each cgroup in the hierarchy. psi_avgs_work has been
    highly optimized, however on systems with large number of cgroups the
    overhead becomes noticeable.
    Systems which use PSI only at the system level could avoid this overhead
    if PSI can be configured to skip per-cgroup stall accounting.
    Add "cgroup_disable=pressure" kernel command-line option to allow
    requesting system-wide only pressure stall accounting. When set, it
    keeps system-wide accounting under /proc/pressure/ but skips accounting
    for individual cgroups and does not expose PSI nodes in cgroup hierarchy.
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Link:  https://lore.kernel.org/patchwork/patch/1435705
    (cherry picked from commit 3958e2d0c34e18c41b60dc01832bd670a59ef70f
     https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git tj)
    
    Conflicts:
            include/linux/cgroup-defs.h
            kernel/cgroup/cgroup.c
    
    1. Trivial merge conflict in cgroup-defs.h due to missing CFTYPE_DEBUG
    2. Changed flags to (CFTYPE_NOT_ON_ROOT | CFTYPE_PRESSURE) in cgroup.c
    because in 4.14 psi files were allowed only in non-root cgroups.
    3. Dropped changes in show_delegatable_files() because it's missing in
    4.14.
    
    Bug: 178872719
    Bug: 191734423
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: Ifc8fbc52f9a1131d7c2668edbb44c525c76c3360
    (cherry-picked from commit d6e8ea926933d8cd7e9df1487a2c063fbeffaea7
    https://android.googlesource.com/kernel/msm/+/d6e8ea926933d8cd7e9df1487a2c063fbeffaea7)
    [jjpprrrr: ported 4.14's kernel/cgroup/cgroup.c changes to
    4.9's kernel/cgroup.c]
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -238,6 +238,22 @@ static struct file_system_type cgroup2_fs_type;
 static struct cftype cgroup_dfl_base_files[];
 static struct cftype cgroup_legacy_base_files[];
 
+/* cgroup optional features */
+enum cgroup_opt_features {
+#ifdef CONFIG_PSI
+	OPT_FEATURE_PRESSURE,
+#endif
+	OPT_FEATURE_COUNT
+};
+
+static const char *cgroup_opt_feature_names[OPT_FEATURE_COUNT] = {
+#ifdef CONFIG_PSI
+	"pressure",
+#endif
+};
+
+static u16 cgroup_feature_disable_mask __read_mostly;
+
 static int rebind_subsystems(struct cgroup_root *dst_root, u16 ss_mask);
 static void cgroup_lock_and_drain_offline(struct cgroup *cgrp);
 static int cgroup_apply_control(struct cgroup *cgrp);
@@ -3565,6 +3581,18 @@ static void cgroup_pressure_release(struct kernfs_open_file *of)
 {
 	psi_trigger_destroy(of->priv);
 }
+
+bool cgroup_psi_enabled(void)
+{
+	return (cgroup_feature_disable_mask & (1 << OPT_FEATURE_PRESSURE)) == 0;
+}
+
+#else /* CONFIG_PSI */
+bool cgroup_psi_enabled(void)
+{
+	return false;
+}
+
 #endif /* CONFIG_PSI */
 
 static ssize_t cgroup_file_write(struct kernfs_open_file *of, char *buf,
@@ -3785,6 +3813,8 @@ static int cgroup_addrm_files(struct cgroup_subsys_state *css,
 restart:
 	for (cft = cfts; cft != cft_end && cft->name[0] != '\0'; cft++) {
 		/* does cft->flags tell us to skip this file on @cgrp? */
+		if ((cft->flags & CFTYPE_PRESSURE) && !cgroup_psi_enabled())
+			continue;
 		if ((cft->flags & __CFTYPE_ONLY_ON_DFL) && !cgroup_on_dfl(cgrp))
 			continue;
 		if ((cft->flags & __CFTYPE_NOT_ON_DFL) && cgroup_on_dfl(cgrp))
@@ -3861,6 +3891,9 @@ static int cgroup_init_cftypes(struct cgroup_subsys *ss, struct cftype *cfts)
 		struct kernfs_ops *kf_ops;
 
 		WARN_ON(cft->ss || cft->kf_ops);
+
+		if ((cft->flags & CFTYPE_PRESSURE) && !cgroup_psi_enabled())
+			continue;
 
 		if (cft->seq_start)
 			kf_ops = &cgroup_kf_ops;
@@ -5003,7 +5036,7 @@ static struct cftype cgroup_dfl_base_files[] = {
 #ifdef CONFIG_PSI
 	{
 		.name = "io.pressure",
-		.flags = CFTYPE_NOT_ON_ROOT,
+		.flags = CFTYPE_NOT_ON_ROOT | CFTYPE_PRESSURE,
 		.seq_show = cgroup_io_pressure_show,
 		.write = cgroup_io_pressure_write,
 		.poll = cgroup_pressure_poll,
@@ -5011,7 +5044,7 @@ static struct cftype cgroup_dfl_base_files[] = {
 	},
 	{
 		.name = "memory.pressure",
-		.flags = CFTYPE_NOT_ON_ROOT,
+		.flags = CFTYPE_NOT_ON_ROOT | CFTYPE_PRESSURE,
 		.seq_show = cgroup_memory_pressure_show,
 		.write = cgroup_memory_pressure_write,
 		.poll = cgroup_pressure_poll,
@@ -5019,7 +5052,7 @@ static struct cftype cgroup_dfl_base_files[] = {
 	},
 	{
 		.name = "cpu.pressure",
-		.flags = CFTYPE_NOT_ON_ROOT,
+		.flags = CFTYPE_NOT_ON_ROOT | CFTYPE_PRESSURE,
 		.seq_show = cgroup_cpu_pressure_show,
 		.write = cgroup_cpu_pressure_write,
 		.poll = cgroup_pressure_poll,
@@ -6297,6 +6330,15 @@ static int __init cgroup_disable(char *str)
 			ss->disabled = 1;
 			printk(KERN_INFO "Disabling %s control group subsystem\n",
 			       ss->name);
+			break;
+		}
+
+		for (i = 0; i < OPT_FEATURE_COUNT; i++) {
+			if (strcmp(token, cgroup_opt_feature_names[i]))
+				continue;
+			cgroup_feature_disable_mask |= 1 << i;
+			pr_info("Disabling %s control group feature\n",
+				cgroup_opt_feature_names[i]);
 			break;
 		}
 	}
