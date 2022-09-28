5caa7689e8274740505df7cb9cf045cbf8cc51dc
Yaroslav Zviezda <acroreiser@gmail.com>
2022-09-28 23:38:33 +0300

    Revert "BACKPORT: ARM: convert all "mov.* pc, reg" to "bx reg" for ARMv6+"
    This reverts commit e9fc28bd67616f57fcab882b4a9024e7ff693f39.

--- arch/arm/lib/bitops.h
+++ arch/arm/lib/bitops.h
@@ -1,4 +1,3 @@
-#include <asm/assembler.h>
 #include <asm/unwind.h>
 
 #if __LINUX_ARM_ARCH__ >= 6
@@ -66,7 +65,7 @@ UNWIND(	.fnstart	)
 	\instr	r2, r2, r3
 	str	r2, [r1, r0, lsl #2]
 	restore_irqs ip
-	ret	lr
+	mov	pc, lr
 UNWIND(	.fnend		)
 ENDPROC(\name		)
 	.endm
@@ -94,7 +93,7 @@ UNWIND(	.fnstart	)
 	\store	r2, [r1]
 	moveq	r0, #0
 	restore_irqs ip
-	ret	lr
+	mov	pc, lr
 UNWIND(	.fnend		)
 ENDPROC(\name		)
 	.endm
