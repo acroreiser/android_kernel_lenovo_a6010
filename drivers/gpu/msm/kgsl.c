74fb543fb455cc3036cc0514112214a9c0881875
Puranam V G Tejaswi <quic_pvgtejas@quicinc.com>
2023-02-14 16:09:02 +0530

    msm: kgsl: Use dma_buf_get() to get dma_buf structure
    Currently we don't ensure if vma->vm_file is associated with dma_buf. This
    can cause issues later when private_data from a non dma_buf file is used as
    dma_buf structure. Hence get the fd that is associated with vma->vm_file
    and use dma_buf_get() to get pointer to dma_buf structure. dma_buf_get()
    ensures that the file from the input fd is associated with dma_buf.
    
    Change-Id: Ib78aef8b16bedca5ca86d3a132278ff9f07dce73
    Signed-off-by: Puranam V G Tejaswi <quic_pvgtejas@quicinc.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -2948,6 +2948,15 @@ out:
 	return ret;
 }
 
+static int match_file(const void *p, struct file *file, unsigned int fd)
+{
+	/*
+	 * We must return fd + 1 because iterate_fd stops searching on
+	 * non-zero return, but 0 is a valid fd.
+	 */
+	return (p == file) ? (fd + 1) : 0;
+}
+
 static void _setup_cache_mode(struct kgsl_mem_entry *entry,
 		struct vm_area_struct *vma)
 {
@@ -2988,6 +2997,8 @@ static int kgsl_setup_useraddr(struct kgsl_mem_entry *entry,
 	vma = find_vma(current->mm, param->hostptr);
 
 	if (vma && vma->vm_file) {
+		int fd;
+
 		ret = check_vma_flags(vma, entry->memdesc.flags);
 		if (ret) {
 			up_read(&current->mm->mmap_sem);
@@ -3003,12 +3014,27 @@ static int kgsl_setup_useraddr(struct kgsl_mem_entry *entry,
 			return -EFAULT;
 		}
 
-		/*
-		 * Take a refcount because dma_buf_put() decrements the
-		 * refcount
-		 */
-		get_file(vma->vm_file);
-		dmabuf = vma->vm_file->private_data;
+		/* Look for the fd that matches this vma file */
+		fd = iterate_fd(current->files, 0, match_file, vma->vm_file);
+		if (fd) {
+			dmabuf = dma_buf_get(fd - 1);
+			if (IS_ERR(dmabuf)) {
+				up_read(&current->mm->mmap_sem);
+				return PTR_ERR(dmabuf);
+			}
+			/*
+			 * It is possible that the fd obtained from iterate_fd
+			 * was closed before passing the fd to dma_buf_get().
+			 * Hence dmabuf returned by dma_buf_get() could be
+			 * different from vma->vm_file->private_data. Return
+			 * failure if this happens.
+			 */
+			if (dmabuf != vma->vm_file->private_data) {
+				dma_buf_put(dmabuf);
+				up_read(&current->mm->mmap_sem);
+				return -EBADF;
+			}
+		}
 	}
 
 	if (!IS_ERR_OR_NULL(dmabuf)) {
