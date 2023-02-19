620842e65da909686714f845be8f36998387a395
Yaroslav Zviezda <acroreiser@gmail.com>
2023-02-20 00:11:46 +0300

    drivers: devfreq: always use Adreno Idler and limit max GPU freq to 400MHz when power saver is active

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -25,6 +25,9 @@
 #include <linux/powersuspend.h>
 #include "governor.h"
 
+// Battery saver
+#include <linux/battery_saver.h>
+
 static bool power_suspended;
 
 static DEFINE_SPINLOCK(tz_lock);
@@ -265,6 +268,10 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 	}
 
 	*freq = devfreq->profile->freq_table[level];
+
+	if (is_battery_saver_on() && *freq > 400000000)
+		*freq = 400000000;
+
 	return 0;
 }
 
