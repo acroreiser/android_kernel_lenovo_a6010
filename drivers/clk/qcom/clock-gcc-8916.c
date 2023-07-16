22c180457e118c82a60db013f27bfe47fe2a5370
Yaroslav Zviezda <acroreiser@gmail.com>
2023-07-16 11:23:12 +0300

    Revert "GPU: Overclock to 550 MHz"
    This reverts commit d8832f4e3102001ccab0fbfa06c375a2433022da.

--- drivers/clk/qcom/clock-gcc-8916.c
+++ drivers/clk/qcom/clock-gcc-8916.c
@@ -565,7 +565,7 @@ static struct rcg_clk vfe0_clk_src = {
 		.dbg_name = "vfe0_clk_src",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP3(LOW, 160000000, NOMINAL, 320000000, HIGH,
-			550000000),
+			465000000),
 		CLK_INIT(vfe0_clk_src.c),
 	},
 };
@@ -598,8 +598,6 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F( 294912000,	   gpll1,   3,	  0,	0),
 	F( 310000000,	   gpll2,   3,	  0,	0),
 	F( 400000000,  gpll0_aux,   2,	  0,	0),
-	F( 465000000,      gpll2,   2,	  0,	0),
-	F( 550000000,      gpll2,   2,    0,    0),
 	F_END
 };
 
@@ -612,8 +610,8 @@ static struct rcg_clk gfx3d_clk_src = {
 	.c = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
-		VDD_DIG_FMAX_MAP3(LOW, 100000000, NOMINAL, 310000000, HIGH,
-			550000000),
+		VDD_DIG_FMAX_MAP3(LOW, 200000000, NOMINAL, 310000000, HIGH,
+			400000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
