a1e0f79d117ec35c4cba17527dcdbda86a068c86
Yaroslav Zviezda <acroreiser@gmail.com>
2023-07-30 11:28:49 +0300

    REVERTME: bpf/seccomp

--- kernel/seccomp.c
+++ kernel/seccomp.c
@@ -68,10 +68,11 @@ struct seccomp_filter {
 static void populate_seccomp_data(struct seccomp_data *sd)
 {
 	struct task_struct *task = current;
+	struct pt_regs *regs = task_pt_regs(current);
 	unsigned long args[6];
 
 	sd->nr = syscall_get_nr(task, regs);
-	sd->arch = syscall_get_arch(task, regs);
+	sd->arch = syscall_get_arch();
 	syscall_get_arguments(task, regs, 0, 6, args);
 	sd->args[0] = args[0];
 	sd->args[1] = args[1];
