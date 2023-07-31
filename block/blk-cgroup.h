18466ac22e23054b8f7ee6c49f37e5ccb3d59fe4
Yaroslav Zviezda <acroreiser@gmail.com>
2023-07-31 22:27:30 +0300

    cfq-iosched: unbreak

--- block/blk-cgroup.h
+++ block/blk-cgroup.h
@@ -241,16 +241,12 @@ static inline struct blkcg_gq *pd_to_blkg(struct blkg_policy_data *pd)
  */
 static inline int blkg_path(struct blkcg_gq *blkg, char *buf, int buflen)
 {
-	char *p;
+	int ret;
 
-	p = cgroup_path(blkg->blkcg->css.cgroup, buf, buflen);
-	if (!p) {
+	ret = cgroup_path(blkg->blkcg->css.cgroup, buf, buflen);
+	if (ret)
 		strncpy(buf, "<unavailable>", buflen);
-		return -ENAMETOOLONG;
-	}
-
-	memmove(buf, p, buf + buflen - p);
-	return 0;
+	return ret;
 }
 
 /**
