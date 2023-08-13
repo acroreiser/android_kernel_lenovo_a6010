8d7b0c067f28768bc90a804886405c7785d00693
Yaroslav Zviezda <acroreiser@gmail.com>
2023-08-13 22:40:05 +0300

    Revert f96572888

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -129,7 +129,12 @@ static bool oom_unkillable_task(struct task_struct *p,
 	if (p->flags & PF_KTHREAD)
 		return true;
 
-	if (p->cred->uid.val < 10000)
+	/* When mem_cgroup_out_of_memory() and p is not member of the group */
+	if (memcg && !task_in_mem_cgroup(p, memcg))
+		return true;
+
+	/* p may not have freeable memory in nodemask */
+	if (!has_intersects_mems_allowed(p, nodemask))
 		return true;
 
 	return false;
