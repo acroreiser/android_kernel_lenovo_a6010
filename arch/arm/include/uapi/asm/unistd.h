3a515c66b5f87872b5d17d0342764a7c8f83d605
Suren Baghdasaryan <surenb@google.com>
2023-12-20 01:45:19 +0300

    BACKPORT: ARM: mm: wire up syscall process_mrelease

--- arch/arm/include/uapi/asm/unistd.h
+++ arch/arm/include/uapi/asm/unistd.h
@@ -417,6 +417,8 @@
 #define __NR_membarrier			(__NR_SYSCALL_BASE+389)
 #define __NR_pidfd_send_signal                 (__NR_SYSCALL_BASE+424)
 #define __NR_pidfd_open			(__NR_SYSCALL_BASE+434)
+#define __NR_process_mrelease                 (__NR_SYSCALL_BASE+448)
+
 /*
  * The following SWIs are ARM private.
  */
