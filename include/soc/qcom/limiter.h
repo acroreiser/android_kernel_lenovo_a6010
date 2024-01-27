b0a1b0c9749cdcff1087969d8350fa99f72e47e4
Yaroslav Zviezda <acroreiser@gmail.com>
2024-01-27 17:06:02 +0300

    Revert "msm_limiter: enable by default"
    This reverts commit 50883562dceb745f17a69aca5ca7bf40256f0130.

--- include/soc/qcom/limiter.h
+++ include/soc/qcom/limiter.h
@@ -38,7 +38,13 @@ static struct cpu_limit {
 	.suspend_max_freq = DEFAULT_SUSPEND_FREQUENCY,
 	.resume_max_freq_all = DEFAULT_RESUME_FREQUENCY,
 	.resume_max_freq[0] = DEFAULT_RESUME_FREQUENCY,
+	.resume_max_freq[1] = DEFAULT_RESUME_FREQUENCY,
+	.resume_max_freq[2] = DEFAULT_RESUME_FREQUENCY,
+	.resume_max_freq[3] = DEFAULT_RESUME_FREQUENCY,
 	.suspend_min_freq_all = DEFAULT_MIN_FREQUENCY,
 	.suspend_min_freq[0] = DEFAULT_MIN_FREQUENCY,
+	.suspend_min_freq[1] = DEFAULT_MIN_FREQUENCY,
+	.suspend_min_freq[2] = DEFAULT_MIN_FREQUENCY,
+	.suspend_min_freq[3] = DEFAULT_MIN_FREQUENCY,
 	.suspend_defer_time = DEFAULT_SUSPEND_DEFER_TIME,
 };
