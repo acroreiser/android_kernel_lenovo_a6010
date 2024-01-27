1b7a7705bbd0892e4306ebfa31762d71dd7b5162
Yaroslav Zviezda <acroreiser@gmail.com>
2024-01-27 14:30:41 +0300

    Revert "staging: LMK: Evaluate regular swap space when calculating memory"
    This reverts commit f36a8a058a8c9a02598ea4afbd5d9d5ad3d545e3.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -136,15 +136,12 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 	int other_free = 0, other_file = 0;
 	unsigned long pressure = action;
 	int array_size = ARRAY_SIZE(lowmem_adj);
-	struct sysinfo swapspace;
 
 	if (!enable_adaptive_lmk)
 		return 0;
 
 	if (pressure >= 95) {
-		si_swapinfo(&swapspace);
-		other_file = swapspace.freeswap +
-			global_page_state(NR_FILE_PAGES) -
+		other_file = global_page_state(NR_FILE_PAGES) -
 			global_page_state(NR_SHMEM) -
 			global_page_state(NR_UNEVICTABLE) -
 			total_swapcache_pages();
@@ -158,9 +155,7 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 		if (lowmem_minfree_size < array_size)
 			array_size = lowmem_minfree_size;
 
-		si_swapinfo(&swapspace);
-		other_file = swapspace.freeswap +
-			global_page_state(NR_FILE_PAGES) -
+		other_file = global_page_state(NR_FILE_PAGES) -
 			global_page_state(NR_SHMEM) -
 			global_page_state(NR_UNEVICTABLE) -
 			total_swapcache_pages();
@@ -394,7 +389,6 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	int other_free;
 	int other_file;
 	unsigned long nr_to_scan = sc->nr_to_scan;
-	struct sysinfo swapspace;
 
 	rcu_read_lock();
 	tsk = current->group_leader;
@@ -411,12 +405,10 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	}
 
 	other_free = global_page_state(NR_FREE_PAGES);
-	si_swapinfo(&swapspace);
 
 	if (global_page_state(NR_SHMEM) + total_swapcache_pages() <
-		global_page_state(NR_FILE_PAGES) + swapspace.freeswap)
-		other_file = swapspace.freeswap +
-			     global_page_state(NR_FILE_PAGES) -
+		global_page_state(NR_FILE_PAGES))
+		other_file = global_page_state(NR_FILE_PAGES) -
 						global_page_state(NR_SHMEM) -
 						global_page_state(NR_UNEVICTABLE) -
 						total_swapcache_pages();
