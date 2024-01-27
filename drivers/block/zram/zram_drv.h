de4dc92570068b19403c4b1594229dc32aa2795e
Yaroslav Zviezda <acroreiser@gmail.com>
2024-01-27 15:13:26 +0300

    Revert "zram: make max_zpage_size tunable"
    This reverts commit 7b2d3d23fb039b48f566e1a2c0f8e2642602db12.

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -30,7 +30,7 @@ u32 zram_pages_stored = 0;
  * Pages that compress to size greater than this are stored
  * uncompressed in memory.
  */
-extern size_t max_zpage_size;
+static const size_t max_zpage_size = PAGE_SIZE / 4 * 3;
 
 /*
  * NOTE: max_zpage_size must be less than or equal to:
