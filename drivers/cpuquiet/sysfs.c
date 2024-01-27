1a7f70fcc8095cf70b83bbcfc8bf362444ba584f
Yaroslav Zviezda <acroreiser@gmail.com>
2024-01-27 16:51:18 +0300

    Revert "cpuquiet: remove 'bool thermal' from store_nr_max_cpus"
    This reverts commit 3e30efc660f8f117d8f2c5456e72bb2e8d84bb76.

--- drivers/cpuquiet/sysfs.c
+++ drivers/cpuquiet/sysfs.c
@@ -159,7 +159,7 @@ static ssize_t store_nr_min_cpus(struct cpuquiet_attribute *cattr,
 }
 
 static ssize_t store_nr_max_cpus(struct cpuquiet_attribute *cattr,
-					const char *buf, size_t count)
+					const char *buf, size_t count, bool thermal)
 {
 	ssize_t ret = 0;
 	unsigned int new_max_cpus;
