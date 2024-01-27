c3eb2d99383bd1a3708289d7ab1ffcaf1959bd86
Yaroslav Zviezda <acroreiser@gmail.com>
2024-01-27 16:45:52 +0300

    Revert "vmstat: increase vmstat interval to 8 seconds"
    This reverts commit 25f9ebbc8fa597d3112feb3da4376cbca5be641e.

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1313,7 +1313,7 @@ static const struct file_operations proc_vmstat_file_operations = {
 
 #ifdef CONFIG_SMP
 static DEFINE_PER_CPU(struct delayed_work, vmstat_work);
-int sysctl_stat_interval __read_mostly = 8 * HZ;
+int sysctl_stat_interval __read_mostly = HZ;
 
 static void vmstat_update(struct work_struct *w)
 {
