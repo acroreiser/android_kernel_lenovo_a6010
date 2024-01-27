91a5dced00803ddaf0c5c0292f08ae8eb27b18c9
Yaroslav Zviezda <acroreiser@gmail.com>
2024-01-27 19:03:00 +0300

    Revert "ANDROID: sdcardfs: update module info"
    This reverts commit 28ae920b7125ff21c8a8a661613d053d2e13a01d.

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -471,15 +471,10 @@ static void __exit exit_sdcardfs_fs(void)
 	pr_info("Completed sdcardfs module unload\n");
 }
 
-/* Original wrapfs authors */
-MODULE_AUTHOR("Erez Zadok, Filesystems and Storage Lab, Stony Brook University (http://www.fsl.cs.sunysb.edu/)");
-
-/* Original sdcardfs authors */
-MODULE_AUTHOR("Woojoong Lee, Daeho Jeong, Kitae Lee, Yeongjin Gil System Memory Lab., Samsung Electronics");
-
-/* Current maintainer */
-MODULE_AUTHOR("Daniel Rosenberg, Google");
-MODULE_DESCRIPTION("Sdcardfs " SDCARDFS_VERSION);
+MODULE_AUTHOR("Erez Zadok, Filesystems and Storage Lab, Stony Brook University"
+	      " (http://www.fsl.cs.sunysb.edu/)");
+MODULE_DESCRIPTION("Wrapfs " SDCARDFS_VERSION
+		   " (http://wrapfs.filesystems.org/)");
 MODULE_LICENSE("GPL");
 
 module_init(init_sdcardfs_fs);
