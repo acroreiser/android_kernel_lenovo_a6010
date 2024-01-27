264903d0f20bd952f80adf1f092ae207cd631a0d
Yaroslav Zviezda <acroreiser@gmail.com>
2024-01-27 16:30:21 +0300

    Revert "locking: rwsem: spin faster"
    This reverts commit ea29d344f5453d5c09bcf15f0625d17dd95dc172.

--- kernel/locking/rwsem-xadd.c
+++ kernel/locking/rwsem-xadd.c
@@ -274,14 +274,12 @@ static inline bool owner_running(struct rw_semaphore *sem,
 static noinline
 bool rwsem_spin_on_owner(struct rw_semaphore *sem, struct task_struct *owner)
 {
-	int i = 0;
 	rcu_read_lock();
 	while (owner_running(sem, owner)) {
 		if (need_resched())
 			break;
 
-		if (i++ > 1000)
-			arch_mutex_cpu_relax();
+		arch_mutex_cpu_relax();
 	}
 	rcu_read_unlock();
 
