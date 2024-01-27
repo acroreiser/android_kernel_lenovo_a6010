eceb337cb8fe0d2f74add2e181c021d8831291cf
Yaroslav Zviezda <acroreiser@gmail.com>
2024-01-27 19:03:17 +0300

    Revert "ANDROID: sdcardfs: Fix style issues with comments"
    This reverts commit d2f62cc9024ba6f399d67aa4a26dd7ce6d8dc887.

--- fs/sdcardfs/dentry.c
+++ fs/sdcardfs/dentry.c
@@ -127,10 +127,12 @@ static int sdcardfs_hash_ci(const struct dentry *dentry,
 	unsigned long hash;
 
 	name = qstr->name;
+	//len = vfat_striptail_len(qstr);
 	len = qstr->len;
 
 	hash = init_name_hash();
 	while (len--)
+		//hash = partial_name_hash(nls_tolower(t, *name++), hash);
 		hash = partial_name_hash(tolower(*name++), hash);
 	qstr->hash = end_name_hash(hash);
 
@@ -145,8 +147,20 @@ static int sdcardfs_cmp_ci(const struct dentry *parent,
 		const struct dentry *dentry, const struct inode *inode,
 		unsigned int len, const char *str, const struct qstr *name)
 {
-	/* FIXME Should we support national language? */
+	/* This function is copy of vfat_cmpi */
+	// FIXME Should we support national language?
+	//struct nls_table *t = MSDOS_SB(parent->d_sb)->nls_io;
+	//unsigned int alen, blen;
 
+	/* A filename cannot end in '.' or we treat it like it has none */
+	/*
+	alen = vfat_striptail_len(name);
+	blen = __vfat_striptail_len(len, str);
+	if (alen == blen) {
+		if (nls_strnicmp(t, name->name, str, alen) == 0)
+			return 0;
+	}
+	*/
 	if (name->len == len) {
 		if (str_n_case_eq(name->name, str, len))
 			return 0;
