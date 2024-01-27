e7804945aed5925d8854bc286cd92508dd939b81
Yaroslav Zviezda <acroreiser@gmail.com>
2024-01-27 14:12:36 +0300

    Revert "block: fiops sync/async scale"
    This reverts commit ea49b3f8d90f0817410811387052a459545adac4.

--- block/fiops-iosched.c
+++ block/fiops-iosched.c
@@ -17,8 +17,6 @@
 
 #define VIOS_READ_SCALE (1)
 #define VIOS_WRITE_SCALE (1)
-#define VIOS_SYNC_SCALE (2)
-#define VIOS_ASYNC_SCALE (5)
 
 struct fiops_rb_root {
 	struct rb_root rb;
@@ -41,8 +39,6 @@ struct fiops_data {
 
 	unsigned int read_scale;
 	unsigned int write_scale;
-	unsigned int sync_scale;
-	unsigned int async_scale;
 };
 
 struct fiops_ioc {
@@ -295,9 +291,6 @@ static u64 fiops_scaled_vios(struct fiops_data *fiopsd,
 	if (rq_data_dir(rq) == WRITE)
 		vios = vios * fiopsd->write_scale / fiopsd->read_scale;
 
-	if (!rq_is_sync(rq))
-		vios = vios * fiopsd->async_scale / fiopsd->sync_scale;
-
 	return vios;
 }
 
@@ -520,8 +513,6 @@ static void *fiops_init_queue(struct request_queue *q)
 
 	fiopsd->read_scale = VIOS_READ_SCALE;
 	fiopsd->write_scale = VIOS_WRITE_SCALE;
-	fiopsd->sync_scale = VIOS_SYNC_SCALE;
-	fiopsd->async_scale = VIOS_ASYNC_SCALE;
 
 	return fiopsd;
 }
@@ -566,8 +557,6 @@ static ssize_t __FUNC(struct elevator_queue *e, char *page)		\
 }
 SHOW_FUNCTION(fiops_read_scale_show, fiopsd->read_scale);
 SHOW_FUNCTION(fiops_write_scale_show, fiopsd->write_scale);
-SHOW_FUNCTION(fiops_sync_scale_show, fiopsd->sync_scale);
-SHOW_FUNCTION(fiops_async_scale_show, fiopsd->async_scale);
 #undef SHOW_FUNCTION
 
 #define STORE_FUNCTION(__FUNC, __PTR, MIN, MAX)				\
@@ -585,8 +574,6 @@ static ssize_t __FUNC(struct elevator_queue *e, const char *page, size_t count)	
 }
 STORE_FUNCTION(fiops_read_scale_store, &fiopsd->read_scale, 1, 100);
 STORE_FUNCTION(fiops_write_scale_store, &fiopsd->write_scale, 1, 100);
-STORE_FUNCTION(fiops_sync_scale_store, &fiopsd->sync_scale, 1, 100);
-STORE_FUNCTION(fiops_async_scale_store, &fiopsd->async_scale, 1, 100);
 #undef STORE_FUNCTION
 
 #define FIOPS_ATTR(name) \
@@ -595,8 +582,6 @@ STORE_FUNCTION(fiops_async_scale_store, &fiopsd->async_scale, 1, 100);
 static struct elv_fs_entry fiops_attrs[] = {
 	FIOPS_ATTR(read_scale),
 	FIOPS_ATTR(write_scale),
-	FIOPS_ATTR(sync_scale),
-	FIOPS_ATTR(async_scale),
 	__ATTR_NULL
 };
 
