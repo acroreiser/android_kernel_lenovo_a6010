f08945c89e8704e6338f8ce99861b21b86f76cd4
Yaroslav Zviezda <acroreiser@gmail.com>
2024-11-10 21:56:06 +0300

    utsname: spoof kernel version for netbpfload
    Fixes V boot.

--- include/linux/utsname.h
+++ include/linux/utsname.h
@@ -79,6 +79,7 @@ static inline struct new_utsname *utsname(void)
 	if (!strcmp(current->comm, "system_server") ||
             !strcmp(current->comm, "zygote") ||
 	    !strcmp(current->comm, "bpfloader") ||
+            !strcmp(current->comm, "netbpfload") ||
             !strcmp(current->comm, "perfetto") ||
             !strcmp(current->comm, "init"))
 	{
