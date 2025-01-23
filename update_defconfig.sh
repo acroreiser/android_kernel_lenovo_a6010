22b9c5e680d70bb88d970a2aa5aa040bf324543d
Yaroslav Zviezda <acroreiser@gmail.com>
2025-01-24 00:01:55 +0300

    update_defconfig: take an argument with device name to update
    In addition to a6010 we have a6000 and sisleyr targets.
    The script must be useful for all supported devices.

--- update_defconfig.sh
+++ update_defconfig.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 if [ -f ".config" ];
 then
-	cat .config > arch/arm/configs/lineageos_a6010_defconfig
-	git add arch/arm/configs/lineageos_a6010_defconfig
-	git commit -m "a6010: update defconfig (scripted)"
+	if [ -f "arch/arm/configs/lineageos_$1_defconfig" ];
+	then
+		cat .config > arch/arm/configs/lineageos_$1_defconfig
+		git add arch/arm/configs/lineageos_$1_defconfig
+		git commit -m "$1: update defconfig (scripted)"
+	else
+		echo "no defconfig for $1, nothing to do"
+	fi
+else
+	echo "no .config, nothing new"
 fi
