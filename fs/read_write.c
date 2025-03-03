dd1ab16210a734805974840478bafe94502bdb5d
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-04 00:31:57 +0800

    KernelSU: scope-minimized manual hooks
    this does not have exec hook yet.
    it will be on next commit after this

--- fs/read_write.c
+++ fs/read_write.c
@@ -501,6 +501,12 @@ static inline void file_pos_write(struct file *file, loff_t pos)
 	file->f_pos = pos;
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_vfs_read_hook __read_mostly;
+extern int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr,
+			size_t *count_ptr);
+#endif
+
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
 	struct fd f = fdget(fd);
@@ -508,6 +514,10 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 
 	if (f.file) {
 		loff_t pos = file_pos_read(f.file);
+#ifdef CONFIG_KSU
+		if (unlikely(ksu_vfs_read_hook)) 
+			ksu_handle_sys_read(fd, &buf, &count);
+#endif
 		ret = vfs_read(f.file, buf, count, &pos);
 		file_pos_write(f.file, pos);
 		fdput(f);
