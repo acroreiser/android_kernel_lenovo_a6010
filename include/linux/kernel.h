19f2689402a9cb7217bf310622c4d5046f1ec5fa
Tejun Heo <tj@kernel.org>
2025-06-05 22:52:17 +0300

    BACKPORT: cgroup: add cgroup_parse_float()
    cgroup already uses floating point for percent[ile] numbers and there
    are several controllers which want to take them as input.  Add a
    generic parse helper to handle inputs.
    
    Update the interface convention documentation about the use of
    percentage numbers.  While at it, also clarify the default time unit.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    
    [@acroreiser: backport to 3.10]

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -112,6 +112,19 @@
 )
 
 /*
+ * Same as above but for u64 dividends. divisor must be a 32-bit
+ * number.
+ */
+#define DIV_ROUND_CLOSEST_ULL(x, divisor)(		\
+{							\
+	typeof(divisor) __d = divisor;			\
+	unsigned long long _tmp = (x) + (__d) / 2;	\
+	do_div(_tmp, __d);				\
+	_tmp;						\
+}							\
+)
+
+/*
  * Multiplies an integer by a fraction, while avoiding unnecessary
  * overflow or loss of precision.
  */
