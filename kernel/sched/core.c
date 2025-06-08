495f81c9eb93346fbfa05d33e40db15bbf8752e6
Yaroslav Zviezda <acroreiser@gmail.com>
2025-06-09 01:18:59 +0300

    sched/uclamp: set default sched_uclamp_util_min to 0

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -912,7 +912,7 @@ static void set_load_weight(struct task_struct *p)
 static DEFINE_MUTEX(uclamp_mutex);
 
 /* Max allowed minimum utilization */
-unsigned int sysctl_sched_uclamp_util_min = SCHED_CAPACITY_SCALE;
+unsigned int sysctl_sched_uclamp_util_min = 0;
 
 /* Max allowed maximum utilization */
 unsigned int sysctl_sched_uclamp_util_max = SCHED_CAPACITY_SCALE;
@@ -1355,6 +1355,9 @@ static void __init init_uclamp(void)
 		root_task_group.uclamp[clamp_id] = uc_max;
 #endif
 	}
+
+	uclamp_se_set(&uclamp_default[UCLAMP_MIN],
+		      sysctl_sched_uclamp_util_min, false);
 }
 
 #else /* CONFIG_UCLAMP_TASK */
