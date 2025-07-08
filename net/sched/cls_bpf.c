0a844d9e6917613be0f14dac7d8efc2225a31625
Yaroslav Zviezda <acroreiser@gmail.com>
2025-07-08 03:03:14 +0300

    cls_bpf: drop INIT_LIST_HEAD_RCU backport

--- net/sched/cls_bpf.c
+++ net/sched/cls_bpf.c
@@ -142,21 +142,6 @@ static bool cls_bpf_is_ebpf(const struct cls_bpf_prog *prog)
 	return !prog->bpf_ops;
 }
 
-/*
- * INIT_LIST_HEAD_RCU - Initialize a list_head visible to RCU readers
- * @list: list to be initialized
- *
- * You should instead use INIT_LIST_HEAD() for normal initialization and
- * cleanup tasks, when readers have no access to the list being initialized.
- * However, if the list being initialized is visible to readers, you
- * need to keep the compiler from being too mischievous.
- */
-static inline void INIT_LIST_HEAD_RCU(struct list_head *list)
-{
-	ACCESS_ONCE(list->next) = list;
-	ACCESS_ONCE(list->prev) = list;
-}
-
 static int cls_bpf_init(struct tcf_proto *tp)
 {
 	struct cls_bpf_head *head;
